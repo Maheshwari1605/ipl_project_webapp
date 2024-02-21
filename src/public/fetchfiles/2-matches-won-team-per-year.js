2dff4d417406e64d73f767cc48f090cb74c0576b
Maheshwari165 <maheshwarivasava165@gmail.com>
2024-02-21 18:24:27 +0530

    heatmap

--- src/public/fetchfiles/2-matches-won-team-per-year.js
+++ src/public/fetchfiles/2-matches-won-team-per-year.js
@@ -38,89 +38,135 @@ fetch("https://iplprojectmb.netlify.app/output/2-matches-won-per-team-per-year.j
 
     // Create the chart
     Highcharts.chart("container", {
-      chart: {
-        type: "heatmap",
-        marginTop: 40,
-        marginBottom: 80,
-        plotBorderWidth: 1,
-      },
+  //     chart: {
+  //       type: "heatmap",
+  //       marginTop: 40,
+  //       marginBottom: 80,
+  //       plotBorderWidth: 1,
+  //     },
 
-      title: {
-        text: "Matches won per team per year",
-        style: {
-          fontSize: "1em",
-        },
-      },
+  //     title: {
+  //       text: "Matches won per team per year",
+  //       style: {
+  //         fontSize: "1em",
+  //       },
+  //     },
 
-      xAxis: {
-        categories: seasonList
-      },
+  //     xAxis: {
+  //       categories: seasonList
+  //     },
 
-      yAxis: {
-        categories: teamname,
-        title: null,
-        reversed: true,
-      },
+  //     yAxis: {
+  //       categories: teamname,
+  //       title: null,
+  //       reversed: true,
+  //     },
 
-      accessibility: {
-        point: {
-          descriptionFormat:
-            "{(add index 1)}. " +
-            "{series.xAxis.categories.(x)} won " +
-            "{series.yAxis.categories.(y)}, {value}.",
-        },
-      },
+  //     accessibility: {
+  //       point: {
+  //         descriptionFormat:
+  //           "{(add index 1)}. " +
+  //           "{series.xAxis.categories.(x)} won " +
+  //           "{series.yAxis.categories.(y)}, {value}.",
+  //       },
+  //     },
 
-      colorAxis: {
-        min: 0,
-        minColor: "#FFFFFF",
-        maxColor: Highcharts.getOptions().colors[0],
-      },
+  //     colorAxis: {
+  //       min: 0,
+  //       minColor: "#FFFFFF",
+  //       maxColor: Highcharts.getOptions().colors[0],
+  //     },
 
-      legend: {
-        align: "right",
-        layout: "vertical",
-        margin: 0,
-        verticalAlign: "top",
-        y: 25,
-        symbolHeight: 280,
-      },
+  //     legend: {
+  //       align: "right",
+  //       layout: "vertical",
+  //       margin: 0,
+  //       verticalAlign: "top",
+  //       y: 25,
+  //       symbolHeight: 280,
+  //     },
 
-      tooltip: {
-        format:
-          "<b>{series.xAxis.categories.(point.x)}</b> matches in <br>" +
-          "<b>{point.value}</b> won <br>" +
-          "<b>{series.yAxis.categories.(point.y)}</b>",
-      },
+  //     tooltip: {
+  //       format:
+  //         "<b>{series.xAxis.categories.(point.x)}</b> matches in <br>" +
+  //         "<b>{point.value}</b> won <br>" +
+  //         "<b>{series.yAxis.categories.(point.y)}</b>",
+  //     },
 
-      series: [
-        {
-          name: "Matches won",
-          borderWidth: 1,
-          data: yearwisearray,
-          dataLabels: {
-            enabled: true,
-            color: "#000000",
-          },
-        },
-      ],
+  //     series: [
+  //       {
+  //         name: "Matches won",
+  //         borderWidth: 1,
+  //         data: yearwisearray,
+  //         dataLabels: {
+  //           enabled: true,
+  //           color: "#000000",
+  //         },
+  //       },
+  //     ],
 
-      responsive: {
-        rules: [
-          {
-            condition: {
-              maxWidth: 500,
-            },
-            chartOptions: {
-              yAxis: {
-                labels: {
-                  format: "{substr value 0 1}",
-                },
-              },
-            },
-          },
-        ],
-      },
-    });
+  //     responsive: {
+  //       rules: [
+  //         {
+  //           condition: {
+  //             maxWidth: 500,
+  //           },
+  //           chartOptions: {
+  //             yAxis: {
+  //               labels: {
+  //                 format: "{substr value 0 1}",
+  //               },
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   });
+  // })
+
+  
+    "chart": {
+      "type": "heatmap",
+      "plotBorderWidth": 1
+    },
+    "title": {
+      "text": "Indian Premier League Team Performance (2008-2017)"
+    },
+    "xAxis": {
+      "categories": seasonList
+    },
+    "yAxis": {
+      "categories": teamname,
+      "title": null
+    },
+    "colorAxis": {
+      "min": 0,
+      "minColor": '#FFFFFF',
+      "maxColor": Highcharts.getOptions().colors[0]
+    },
+    "legend": {
+      "align": 'right',
+      "layout": 'vertical',
+      "margin": 0,
+      "verticalAlign": 'top',
+      "y": 25,
+      "symbolHeight": 280
+    },
+    "tooltip": {
+      "formatter": function () {
+        return '<b>' + this.series.yAxis.categories[this.point.y] + '</b> won <br><b>' +
+          this.point.value + '</b> matches in <br><b>' + this.series.xAxis.categories[this.point.x] + '</b>';
+      }
+    },
+    "series": [{
+      "name": "Matches Won",
+      "borderWidth": 1,
+      "data": yearwisearray,
+      "dataLabels": {
+        "enabled": true,
+        "color": '#000000'
+      }
+    }]
   })
+})
   .catch((error) => console.error("Error fetching data:", error));
